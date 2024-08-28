63275c8e4f993a3006e1aa475621e6348ad78eec
xipengwang-alex <wang4706@purdue.edu>
2024-08-28 17:26:26 -0400

    Updated setup4 statement

--- client/src/components/Setup4.js
+++ client/src/components/Setup4.js
@@ -6,11 +6,36 @@ function Setup4({ formData, setFormData, showSuccess = false}) {
     <div className="content">
       <h1>Your Resume Book Profile</h1>
 
-      <br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/>
+      {!showSuccess && [...Array(6)].map((_, i) => <br key={i} />)}
+
+      {!showSuccess && <p>Thank you for completing your profile in Resume Book!</p>}
       
       {showSuccess && <p>Success!</p>}
+      
 
-      <p>Thank you for completing your profile in Resume Book!</p>
+      {showSuccess && 
+      <div style={{ maxWidth: '800px', margin: '10px auto', padding: '10px', borderRadius: '8px' }}>
+        <p>Additionally, please submit your resume to <strong>actuary@purdue.edu</strong> with the subject: <strong>Resume</strong>.</p>
+        
+        <p style={{ color: 'red', fontWeight: 'bold' }}>
+          It must be saved with this name format:
+        </p>
+        
+        <p style={{
+          backgroundColor: '#fff',
+          padding: '10px',
+          borderRadius: '8px',
+          wordWrap: 'break-word',
+          overflowWrap: 'break-word',
+          hyphens: 'auto'
+        }}>
+          LastName_FirstName_Class_Internship/FullTime_PurdueEmailPrefix_P_FM(PassedExams)_Sponsorship(only required for international students).pdf
+        </p>
+        
+        <p>If you have any questions, please email the Purdue Actuarial Club at <strong>actuary@purdue.edu</strong></p>
+      </div>
+      }
 
     </div>
   );
