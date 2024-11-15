55581fb34fb95015f4edaf8e3c29795dc45cd652
xipengwang-alex <wang4706@purdue.edu>
2024-11-15 07:53:30 -0500

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use('/actuarial_science', express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(cors(corsOptions));
@@ -219,7 +219,7 @@ app.post(
 app.use('/resumes', express.static(path.join(__dirname, 'resumes')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/actuarial_science/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
